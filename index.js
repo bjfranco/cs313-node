1ce3df35aadb07bee6bb4df55706f6f21c28ba18
bjfranco <fra18020@byui.edu>
2020-11-13 16:34:04 -0700

    Updating Prove09

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/prove09' , (req, res) => res.sendFile(__dirname + '/public/prove09-postal.html'));
+  .get('/prove09' , (req, res) => res.sendFile(__dirname + '/public/prove09-postal.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
